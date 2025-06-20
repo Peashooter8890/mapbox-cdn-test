5ead425a3eef2d6e763b1404e4e8eda7038b300d
Peashooter8890 <peashooterking8@gmail.com>
2025-06-19 23:49:40 -0400

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import MapComponent from './MapComponent';
 
 // This is the crucial part. We are not mounting anything here.
 // We are simply making the component available on a global object.
-window.MapboxApp = {
-  Component: MapComponent,
-};
+export { MapComponent };
