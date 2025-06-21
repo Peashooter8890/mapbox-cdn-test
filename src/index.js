d090bec2522eeccdedb67b9c7a0af8d101665f6e
Peashooter8890 <peashooterking8@gmail.com>
2025-06-21 00:36:38 -0400

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MapComponent from './MapComponent';
 
+// Define the custom element only once
 class MapWebComponent extends HTMLElement {
+  constructor() {
+    super();
+    // Create a shadow root once in the constructor
+    this._root = this.attachShadow({ mode: 'open' });
+  }
+
   connectedCallback() {
-    ReactDOM.render(<MapComponent />, this.attachShadow({ mode: 'open' }));
+    // Render React component into the shadow root
+    ReactDOM.render(<MapComponent />, this._root);
+  }
+
+  disconnectedCallback() {
+    // Clean up when the element is removed
+    ReactDOM.unmountComponentAtNode(this._root);
   }
 }
-customElements.define('map-component', MapWebComponent);
+
+// Register the custom element if not already defined
+if (!customElements.get('map-component')) {
+  customElements.define('map-component', MapWebComponent);
+}
